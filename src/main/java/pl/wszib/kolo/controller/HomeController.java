c2034bc5bd2255d4b2a507d1cf27956745eb3402
Marek Kasztelnik <mkasztelnik@gmail.com>
2014-03-02 12:29:50 +0100

    spring mvc

---
+++ src/main/java/pl/wszib/kolo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package pl.wszib.kolo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+ 
+		model.addAttribute("message", "Spring 4 MVC Hello World");
+		return "home";
+	}
+}
